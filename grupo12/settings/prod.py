aa90a7106422318de036c1925e0819c138d01b65
Marcos Eduardo Eguiazabal <98111090+Marcos316424@users.noreply.github.com>
2022-10-10 14:02:34 -0300

    Update

--- grupo12/settings/prod.py
+++ grupo12/settings/prod.py
@@ -10,10 +10,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
         'ENGINE': 'mssql',
-        'NAME':'db65kq741noh2c',
-        'USER': 'wxeumdtcmognir',
-        'PASSWORD': '7d08c4a9c7ca74e5ab7ff6fb6bfcf4d7a3ca7e96d391e9b48a5c1400a3a3d7a2',
-        'HOST': 'ec2-44-207-126-176.compute-1.amazonaws.com',
+        'NAME':'ddrmlbjn0p67md',
+        'USER': 'vqamoaupfutgjh',
+        'PASSWORD': '3c2515f524d6628d0abe9d31773cc58858df558eb70acc093b9e4279ddd58f70',
+        'HOST': 'ec2-3-219-19-205.compute-1.amazonaws.com',
         'PORT': '5432',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
